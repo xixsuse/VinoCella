6e088be8d0eb0a868c213f2dd7495c68eeef6f73
Andrew <aajmarshall@hotmail.com>
2017-07-24 07:32:46 +1000

    Added an alert dialog fragment to request a user input the wine tasting name.

--- app/src/main/java/com/example/android/cellavino/UserInterface2/CreateTasting.java
+++ app/src/main/java/com/example/android/cellavino/UserInterface2/CreateTasting.java
@@ -3,7 +3,11 @@ package com.example.android.cellavino.UserInterface2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android.cellavino.MainActivity;
 import com.example.android.cellavino.R;
@@ -12,7 +16,10 @@ import com.example.android.cellavino.R;
  * Created by Andrew on 15/07/2017.
  */
 
+
 public class CreateTasting extends MainActivity {
+
+    private Button mCreateTastingButton;
 
     public CreateTasting() {
     }
@@ -29,9 +36,30 @@ public class CreateTasting extends MainActivity {
         addWineFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CreateTasting.this, NewTasting.class);
-                startActivity(intent);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(CreateTasting.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_create_tasting, null);
+                final EditText mTastingNameInput = (EditText) mView.findViewById(R.id.dialog_create_tasting_name_input);
+                Button mCreateTastingButton = (Button) findViewById(R.id.create_tasting_name_button);
+
+                /*
+                mCreateTastingButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!mTastingNameInput.getText().toString().isEmpty()) {
+                            Toast.makeText(CreateTasting.this, "Field Complete", Toast.LENGTH_SHORT).show();
+                        } if (mTastingNameInput.getText().toString().isEmpty()) {
+                            Toast.makeText(CreateTasting.this, "Field Empty", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                */
+
+                alertDialogBuilder.setView(mView);
+                AlertDialog dialog = alertDialogBuilder.create();
+                dialog.show();
+
             }
+
         });
     }
 
--- app/src/main/java/com/example/android/cellavino/UserInterface2/NewTasting.java
+++ app/src/main/java/com/example/android/cellavino/UserInterface2/NewTasting.java
@@ -1,8 +1,18 @@
 package com.example.android.cellavino.UserInterface2;
 
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.StyleRes;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Andrew on 22/07/2017.
  */
 
-public class NewTasting {
+public class NewTasting extends AppCompatActivity {
+    //@Override
+    //protected void onCreate(Bundle savedInstanceState){
+    //super.onCreate(savedInstanceState);
+    //    setContentView(android.support.design.R.layout);
+    //}
 }
